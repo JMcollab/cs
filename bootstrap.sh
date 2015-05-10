3b8bd9a17d6bcfacee0969e8a3a39ea29293ced3
Mitchell Valine <mitchellvaline@gmail.com>
2015-05-10 14:45:20 -0700

    vagrant machine now inits and activates virtualenv and installs python dependencies from requirements.txt, added user.html template for testing, settings imports local_settings file for local testing and env variables

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,21 @@
+#!/usr/bin/env bash
+
 apt-get update
 apt-get install -y python3-pip python3-dev libpq-dev git
-pip3 install django
-pip3 install django-toolbelt
+pip3 install virtualenv
+
+# aliases for django commands
 echo "alias djrun=\"python3 manage.py runserver 0.0.0.0:8000\"" >> .bashrc
 echo "alias python=\"python3\"" >> .bashrc
 echo "alias manage=\"python3 manage.py\"" >> .bashrc
 echo "alias pip=\"pip3\"" >> .bashrc
+
+# setting up vitualenv
+mkdir /home/vagrant/.virtualenvs
+virtualenv -p python3 /home/vagrant/.virtualenvs/proj
+source /home/vagrant/.virtualenvs/proj/bin/activate
+
+# install project requirements to virtualenv
+cd /vagrant && pip install -r requirements.txt
+
+echo "source ~/.virtualenvs/proj/bin/activate" >> /home/vagrant/.bashrc
