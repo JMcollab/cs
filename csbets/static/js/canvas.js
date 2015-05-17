4f2edbf402d42828d50f4e16bc925e5417433418
Mitchell Valine <mitchellvaline@gmail.com>
2015-05-17 16:31:48 -0700

    skinstext innerhtml get working

--- csbets/static/js/canvas.js
+++ csbets/static/js/canvas.js
@@ -10,11 +10,12 @@ ctx.fillRect(0,0, 200, -600);
 
 /*var interval = window.setInterval(fillBar, 1000); *///Fills bar by 1 per second...doesn't stop
 
+var skinstext = document.getElementById("numSkins");
 function fillBar() {
     ctx.fillStyle="#14a1ff";
     ctx.fillRect(0, 0, 200, value * -10); //Up to 10 skins
     if(i % 6 == 0)
-        skins.innerHTML = (i/6).toString() + " skins deposited";
+        skinstext.innerHTML = (i/6).toString() + " skins deposited";
     if(i <= skins*6) {
         i++;
         value ++;
